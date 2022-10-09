1e545543fcca8473f655500c577a7366bc5366d2
司俊峰 <15283666096@163.com>
2022-10-09 17:41:26 +0800

    add:添加移动端手势滑动监听

--- src/ReactSimpleImageViewer.tsx
+++ src/ReactSimpleImageViewer.tsx
@@ -16,6 +16,11 @@ interface IProps {
 const ReactSimpleImageViewer = (props: IProps) => {
   const [currentIndex, setCurrentIndex] = useState(props.currentIndex ?? 0);
 
+  let startX = 0;
+  let startY = 0;
+  let endX = 0;
+  let endY = 0;
+
   const changeImage = useCallback(
     (delta: number) => {
       let nextIndex = (currentIndex + delta) % props.src.length;
@@ -31,8 +36,8 @@ const ReactSimpleImageViewer = (props: IProps) => {
         return;
       }
 
-      const checkId = event.target.id === 'ReactSimpleImageViewer';
-      const checkClass = event.target.classList.contains('react-simple-image-viewer__slide');
+      const checkId = event.target.id === "ReactSimpleImageViewer";
+      const checkClass = event.target.classList.contains("react-simple-image-viewer__slide");
 
       if (checkId || checkClass) {
         event.stopPropagation();
@@ -53,6 +58,7 @@ const ReactSimpleImageViewer = (props: IProps) => {
       }
 
       if (["ArrowRight", "l"].includes(event.key)) {
+        console.log(1231321);
         changeImage(1);
       }
     },
@@ -64,21 +70,61 @@ const ReactSimpleImageViewer = (props: IProps) => {
       if (event.wheelDeltaY > 0) {
         changeImage(-1);
       } else {
+        console.log("scroll");
         changeImage(1);
       }
     },
     [changeImage]
   );
 
+  const handleTouchStart = useCallback(
+    (event: TouchEvent) => {
+      event.preventDefault();
+      startX = event.changedTouches[0].pageX;
+      startY = event.changedTouches[0].pageY;
+    },
+    [changeImage]
+  );
+
+  const handleTouchEnd = useCallback(
+    (event: TouchEvent) => {
+      event.preventDefault();
+      endX = event.changedTouches[0].pageX;
+      endY = event.changedTouches[0].pageY;
+
+      const x = startX - endX;
+      const Y = startY - endY;
+      if (Math.abs(x) > Math.abs(Y) && x > 0) {
+        // move to left
+        changeImage(1);
+      } else if (Math.abs(x) > Math.abs(Y) && x < 0) {
+        // move to right
+        changeImage(-1);
+      } else if (Math.abs(Y) > Math.abs(x) && Y > 0) {
+        // move to up
+        props.onClose?.();
+      } else if (Math.abs(Y) > Math.abs(x) && Y < 0) {
+        // move to bottom
+        props.onClose?.();
+      }
+    },
+    [props.onClose, changeImage]
+  );
+
   useEffect(() => {
+    let box = document.querySelector("#ReactSimpleImageViewer") as Element;
     document.addEventListener("keydown", handleKeyDown);
+    box.addEventListener("touchstart", handleTouchStart);
+    box.addEventListener("touchend", handleTouchEnd);
 
     if (!props.disableScroll) {
       document.addEventListener("wheel", handleWheel);
     }
 
     return () => {
-      document.removeEventListener("keydown", handleKeyDown);
+      box.removeEventListener("keydown", handleKeyDown);
+      box.removeEventListener("touchstart", handleTouchStart);
+      document.removeEventListener("touchend", handleTouchEnd);
 
       if (!props.disableScroll) {
         document.removeEventListener("wheel", handleWheel);
@@ -94,35 +140,23 @@ const ReactSimpleImageViewer = (props: IProps) => {
       onClick={handleClick}
       style={props.backgroundStyle}
     >
-      <span
-        className={`${styles.close} react-simple-image-viewer__close`}
-        onClick={() => props.onClose?.()}
-      >
-        { props.closeComponent || "×" }
+      <span className={`${styles.close} react-simple-image-viewer__close`} onClick={() => props.onClose?.()}>
+        {props.closeComponent || "×"}
       </span>
 
       {props.src.length > 1 && (
-        <span
-          className={`${styles.navigation} ${styles.prev} react-simple-image-viewer__previous`}
-          onClick={() => changeImage(-1)}
-        >
-          { props.leftArrowComponent || "❮" }
+        <span className={`${styles.navigation} ${styles.prev} react-simple-image-viewer__previous`} onClick={() => changeImage(-1)}>
+          {props.leftArrowComponent || "❮"}
         </span>
       )}
 
       {props.src.length > 1 && (
-        <span
-          className={`${styles.navigation} ${styles.next} react-simple-image-viewer__next`}
-          onClick={() => changeImage(1)}
-        >
-          { props.rightArrowComponent || "❯" }
+        <span className={`${styles.navigation} ${styles.next} react-simple-image-viewer__next`} onClick={() => changeImage(1)}>
+          {props.rightArrowComponent || "❯"}
         </span>
       )}
 
-      <div
-        className={`${styles.content} react-simple-image-viewer__modal-content`}
-        onClick={handleClick}
-      >
+      <div className={`${styles.content} react-simple-image-viewer__modal-content`} onClick={handleClick}>
         <div className={`${styles.slide} react-simple-image-viewer__slide`}>
           <img className={styles.image} src={props.src[currentIndex]} alt="" />
         </div>
